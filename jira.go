272cf01c2d9f900d067ec6a4632f1d1556608569
seeker815 <ephemeral972@gmail.com>
2018-03-03 19:08:34 +0530

    Set init for jira login, modified the SHA readability and added git pull

---
+++ jira.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	jira "github.com/andygrunwald/go-jira"
+)
+
+func init() {
+	var err error
+
+	jiraClient, err = jira.NewClient(nil, os.Getenv("JIRA_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	// $ JIRA_USER=xxxxx JIRA_PASS=xxxx ./program
+
+	username, password := os.Getenv("JIRA_USER"), os.Getenv("JIRA_PASS")
+	jiraClient.Authentication.SetBasicAuth(username, password)
+}
--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"os"
 	"regexp"
 
-	"github.com/andygrunwald/go-jira"
-	"github.com/google/go-github/github"
+	jira "github.com/andygrunwald/go-jira"
+	github "github.com/google/go-github/github"
+)
+
+var (
+	jiraClient *jira.Client
+	hashSHA    string
+	pubKey     string
+	gitUser    string
 )
 
 //User struct to manage user credentials
@@ -22,13 +29,7 @@ type User struct {
 }
 
 func main() {
-	jiraClient, err := jira.NewClient(nil, "<jira-url>")
-	if err != nil {
-		panic(err)
-	}
-	jiraClient.Authentication.SetBasicAuth("usernamem", "password")
-
-	issue, _, err := jiraClient.Issue.Get("IXE-9580", nil)
+	issue, _, err := jiraClient.Issue.Get("IXE-9575", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -44,34 +45,30 @@ func main() {
 	if len(cmntList) == 0 {
 		fmt.Println("No Pubkey/password has been entered, nil!")
 	} else {
-		for i := range cmntList {
-			cmnt := cmntList[i]
+		for _, cmnt := range cmntList {
 			fmt.Println(cmnt.Body)
+
 			matchedPubkey, _ := regexp.MatchString("^ssh-rsa\\s\\S*\\s[a-zA-Z0-9-@]*", cmnt.Body)
 			if matchedPubkey == true {
 				pubKey := cmnt.Body
 				fmt.Println("SSH Pub Key:", pubKey)
-			} else {
-				//shaHash, _ := ioutil.ReadFile(cmnt.Body)
-				test := "VdGttKB9EsLdRxZW.bsRXow3Rx2uMhFFwUL4sHFFfgmcKMBRUJGLzXPe6pLErKuIYHhx439OIHuAr1ZuUmSSA."
-				data, _ := base64.StdEncoding.DecodeString(test)
+				continue
+			}
 
-				//fmt.Printf("Sha512: %x\n\n", sha512.Sum512(shaHash))
-				fmt.Println("sha checksum", data)
+			matchedHash, _ := regexp.MatchString("^[0-9a-zA-Z\\S]+$", cmnt.Body)
+			if matchedHash == true {
+				hashSHA := cmnt.Body
+				fmt.Println("Matched SHA512:", hashSHA)
+				continue
 			}
 
 		}
 	}
 
-	mb := User{"true", "hello", "ssh-rsa", "bash", "present", "nikil"}
-	fmt.Println(mb)
-	userJSON, _ := json.Marshal(mb)
-	fmt.Println(string(userJSON))
-
-	/* Git login */
+	username, token := os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_TOKEN")
 	tp := github.BasicAuthTransport{
-		Username: "git-user",
-		Password: "git-token",
+		Username: username,
+		Password: token,
 	}
 
 	gitClient := github.NewClient(tp.Client())
@@ -79,42 +76,28 @@ func main() {
 	user, _, err := gitClient.Users.Get(ctx, "")
 
 	if err != nil {
-		fmt.Printf("\nerror: %v\n", err)
-		return
+		panic(err)
 	}
+
+	mb := User{"true", hashSHA, pubKey, "bash", "present", issue.Fields.Assignee.Name}
+	fmt.Println(mb)
+	userJSON, _ := json.Marshal(mb)
+	fmt.Println(string(userJSON))
 
 	fmt.Printf("\n%v\n", github.Stringify(user.Login))
 	fmt.Printf("\n%v\n", github.Stringify(user.OrganizationsURL))
-
-	// PUT update the main.yml file
 	s := github.Stringify(user.Login)
-	s = s[1 : len(s)-1]
-	//response, status, err := gitClient.Repositories.GetCommit(ctx, s, "s3-registry", "b6fc75e29cd194be301847f879cbcf09c04a7926")
-	var commitMsg *string
-	str1 := "Commit Msgs"
-	commitMsg = &str1
+	gitUser := s[1 : len(s)-1]
 
-	author := github.CommitAuthor{
-		Name:  user.Name,
-		Email: user.Email,
-		Login: user.Login,
+	rcg := &github.RepositoryContentGetOptions{Ref: "master"}
+
+	// git pull
+	fc, rc, resp, err := gitClient.Repositories.GetContents(ctx, gitUser, "s3-registry", "hooks/build", rcg)
+	if err != nil {
+		fmt.Printf("\nGithub pull failed: %v\n", err)
 	}
 
-	authorRef := &author
-	sha := "55dc6114b38fd123be0b3221c53541de7a73bea7"
+	fmt.Println("Git response", fc, rc, resp)
 
-	rc := github.RepositoryContentFileOptions{
-		Message:   commitMsg,
-		Content:   userJSON,
-		SHA:       &sha,
-		Committer: authorRef,
-	}
-
-	//fmt.Println("Status of commit thru api: ", response, status, err)
-	var rcp *github.RepositoryContentFileOptions
-	rcp = &rc
-	response, status, err := gitClient.Repositories.UpdateFile(ctx, s, "s3-registry", "hooks/build", rcp)
-	fmt.Println("Commit status", response, status, err)
+	
 }
-
-/* Post comment on Jira with commits */
