ca5d598286332b6dab063f44c0b2c548f62b7a1d
seeker815 <ephemeral972@gmail.com>
2018-06-03 23:25:26 +0530

    Modularize as much as possible

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	jira "github.com/andygrunwald/go-jira"
 	github "github.com/google/go-github/github"
+	log "github.com/sirupsen/logrus"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -23,6 +24,7 @@ var (
 	gitRepo    string
 	gitPath    string
 	mb         User
+	xc         *github.RepositoryContent
 )
 
 //User struct to manage user credentials
@@ -35,7 +37,94 @@ type User struct {
 	Username       string   `yaml:"username"`
 }
 
+// createUserCred parses jira comments for Pub key and Password hash
+func createUserCred(cmntList []*jira.Comment) ([]string, string) {
+	for _, cmnt := range cmntList {
+		matchedPubkey, _ := regexp.MatchString("^ssh-rsa\\s\\S*\\s[a-zA-Z0-9-@]*", cmnt.Body)
+		if matchedPubkey == true {
+			pubKey = []string{strings.TrimSpace(cmnt.Body)}
+			fmt.Println(pubKey)
+			continue
+		}
+
+		matchedHash, _ := regexp.MatchString("^[0-9a-zA-Z\\S]+$", cmnt.Body)
+		if matchedHash == true {
+			hashSHA = cmnt.Body
+			fmt.Println(hashSHA)
+			continue
+		}
+
+	}
+	return pubKey, hashSHA
+}
+
+// gitFetch fetches git content/file for the repo path
+func gitFetch(repo string, filePath string) (*github.RepositoryContent, error) {
+	username, token := os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_TOKEN")
+	tp := github.BasicAuthTransport{
+		Username: username,
+		Password: token,
+	}
+	gitClient := github.NewClient(tp.Client())
+	ctx := context.Background()
+	user, _, err := gitClient.Users.Get(ctx, "")
+	if err != nil {
+		panic(err)
+	}
+	log.Info("\n%v\n", github.Stringify(user.Login))
+	log.Info("\n%v\n", github.Stringify(user.OrganizationsURL))
+	s := github.Stringify(user.Login)
+	gitUser := s[1 : len(s)-1]
+	rcg := &github.RepositoryContentGetOptions{Ref: "master"}
+	fc, _, _, err := gitClient.Repositories.GetContents(ctx, gitUser, repo, filePath, rcg)
+	return fc, err
+}
+
+func gitPush(repo string, filePath string, jiraID string, fc *github.RepositoryContent, y []byte) (*github.RepositoryContentResponse, error) {
+	username, token := os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_TOKEN")
+	tp := github.BasicAuthTransport{
+		Username: username,
+		Password: token,
+	}
+	gitClient := github.NewClient(tp.Client())
+	ctx := context.Background()
+	user, _, err := gitClient.Users.Get(ctx, "")
+	if err != nil {
+		panic(err)
+	}
+	log.Info("\n%v\n", github.Stringify(user.Login))
+	log.Info("\n%v\n", github.Stringify(user.OrganizationsURL))
+	s := github.Stringify(user.Login)
+	gitUser := s[1 : len(s)-1]
+
+	author := &github.CommitAuthor{
+		Name:  user.Name,
+		Email: user.Email,
+		Login: user.Login,
+	}
+
+	commitMsg := fmt.Sprintf("Updating user credentials for %s", jiraID)
+
+	rcp := &github.RepositoryContentFileOptions{
+		Message:   &commitMsg,
+		Content:   y,
+		SHA:       fc.SHA,
+		Committer: author,
+	}
+
+	response, _, err := gitClient.Repositories.UpdateFile(ctx, gitUser, repo, filePath, rcp)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Commit status", response)
+	return response, err
+}
+
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.WarnLevel)
+
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter JIRA ID for user credentials: ")
 	ji, _ := reader.ReadString('\n')
@@ -54,125 +143,53 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	log.WithFields(log.Fields{
+		"Name":     issue.Fields.Type.Name,
+		"Summary":  issue.Fields.Summary,
+		"key":      issue.Fields.Assignee.Name,
+		"Priority": issue.Fields.Priority.Name,
+	}).Info("Fetched JIRA attributes")
 
-	fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
-	fmt.Printf("Type: %s\n", issue.Fields.Type.Name)
-	fmt.Printf("Priority: %s\n", issue.Fields.Priority.Name)
-	fmt.Printf("Assignee: %s\n", issue.Fields.Assignee.Name)
-
-	cmntList := issue.Fields.Comments.Comments
-	if len(cmntList) == 0 {
+	if len(issue.Fields.Comments.Comments) == 0 {
 		fmt.Println("No Pubkey/password has been entered, nil!")
-	} else {
-		for _, cmnt := range cmntList {
-			fmt.Println(cmnt.Body)
-
-			matchedPubkey, _ := regexp.MatchString("^ssh-rsa\\s\\S*\\s[a-zA-Z0-9-@]*", cmnt.Body)
-			if matchedPubkey == true {
-				pubKey =  []string{strings.TrimSpace(cmnt.Body)}
-				fmt.Println(pubKey)
-				continue
-			}
-
-			matchedHash, _ := regexp.MatchString("^[0-9a-zA-Z\\S]+$", cmnt.Body)
-			if matchedHash == true {
-				hashSHA = cmnt.Body
-				continue
-			}
-
-		}
-		mb = User{"true", hashSHA, pubKey, "bash", "present", issue.Fields.Assignee.Name}
-		userJSON, err = yaml.Marshal(mb)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-			return
-		}
-		fmt.Println(string(userJSON))
+		return
 	}
 
-	username, token := os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_TOKEN")
-	tp := github.BasicAuthTransport{
-		Username: username,
-		Password: token,
-	}
-
-	gitClient := github.NewClient(tp.Client())
-	ctx := context.Background()
-	user, _, err := gitClient.Users.Get(ctx, "")
-
+	pubKey, hashSHA = createUserCred(issue.Fields.Comments.Comments)
+	mb = User{"true", hashSHA, pubKey, "bash", "present", issue.Fields.Assignee.Name}
+	userJSON, err = yaml.Marshal(mb)
 	if err != nil {
-		panic(err)
+		log.Error("Unable to connect to local syslog daemon:", err)
+		return
 	}
 
-	fmt.Printf("\n%v\n", github.Stringify(user.Login))
-	fmt.Printf("\n%v\n", github.Stringify(user.OrganizationsURL))
-	s := github.Stringify(user.Login)
-	gitUser := s[1 : len(s)-1]
-
-	rcg := &github.RepositoryContentGetOptions{Ref: "master"}
-
-	fc, _, _, err := gitClient.Repositories.GetContents(ctx, gitUser, gitRepo, gitPath, rcg)
+	xc, err = gitFetch(gitRepo, gitPath)
 	if err != nil {
-		fmt.Printf("\nGithub pull failed: %v\n", err)
+		log.Error("Github pull failed", err)
 	}
 
-	sDec, err := b64.StdEncoding.DecodeString(*fc.Content)
-
+	sDec, err := b64.StdEncoding.DecodeString(*xc.Content)
 	originalYaml := struct {
 		UserList []User `yaml:"user_list"`
 	}{}
 
-	fmt.Println("content", string(sDec))
+	log.Info("content", string(sDec))
 
 	err = yaml.Unmarshal(sDec, &originalYaml)
 	if err != nil {
-		fmt.Println("Error unmarshalling yaml: ", err)
+		log.Error("Error unmarshalling yaml: ", err)
 	}
 
 	originalYaml.UserList = append(originalYaml.UserList, mb)
-
 	b, _ := yaml.Marshal(originalYaml)
 
-	//fmt.Println("content", string(sDec))
-	fmt.Println("new content\n", string(b))
+	fmt.Printf("\n content %v\n", string(sDec))
+	fmt.Printf("\n new content %v\n", string(b))
 
-	// sDecs := append(sDec, userJSON...)
-	// fmt.Println("appended", string(sDecs))
+	r, err := gitPush(gitRepo, gitPath, jiraID, xc, b)
+	if err != nil {
+		log.Error("Error in Git Push : ", err)
+	}
 
-	// y, err := yaml2.JSONToYAML(sDec)
-	// if err != nil {
-	// 	fmt.Printf("err: %v\n", err)
-	// 	return
-	// }
-	// fmt.Println("content in yaml", string(y))
-	// ya := append(y, userJSON...)
-	// fmt.Println("appended", string(ya))
-
-	// yy, err := yaml2.JSONToYAML(ya)
-	// if err != nil {
-	// 	fmt.Printf("err: %v\n", err)
-	// 	return
-	// }
-	// fmt.Println("-----final string", string(yy))
-	// author := &github.CommitAuthor{
-	// 	Name:  user.Name,
-	// 	Email: user.Email,
-	// 	Login: user.Login,
-	// }
-
-	// commitMsg := fmt.Sprintf("Updating user credentials for %s", jiraID)
-
-	// rcp := &github.RepositoryContentFileOptions{
-	// 	Message:   &commitMsg,
-	// 	Content:   y,
-	// 	SHA:       fc.SHA,
-	// 	Committer: author,
-	// }
-
-	// response, _, err := gitClient.Repositories.UpdateFile(ctx, gitUser, gitRepo, gitPath, rcp)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("Commit status", response)
-
+	log.Info("Git push response", r)
 }
